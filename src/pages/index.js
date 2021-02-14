70598a67e7c11c2d01a1bde13f62039b24ffce0b
MaxwellKJr <maxwellkjr@gmail.com>
2021-02-14 10:44:33 +0200

    official download link update

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,9 +32,12 @@ const Index = () => {
             <a
               href="https://www.datafilehost.com/d/d9671037"
               className="waves-effect waves-dark btn btn-large amber lighten-2 black-text"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Download PDF <FontAwesomeIcon icon={faDownload} />
             </a>
+
             <Link href="/about-book">
               <a className="waves-effect waves-dark btn btn-large amber lighten-2 black-text b-text">
                 About Book <FontAwesomeIcon icon={faArrowRight} />
